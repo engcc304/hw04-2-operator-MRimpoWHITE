d99a1f0ad98e561fed621d15f8ff361e39aba489
MRimpoWHITE <phichitphu2015@gmail.com>
2023-08-05 01:14:50 +0700

    123

--- Homework/HW04/hw04-2-operator.cpp
+++ Homework/HW04/hw04-2-operator.cpp
@@ -4,11 +4,11 @@
 #include <stdio.h>
 int main() {
     int a = 0, b = 0 ;
-    printf( "Please enter value (a b) : " ) ;
+    printf( "Please enter value (a b) :" ) ;
     scanf( "%d %d", &a, &b ) ; //TODO: #11 Input variable 'a' and 'b'
 
     //TODO: #12 Complete operator, and display all operator output. (and fix all bugs.)
-    printf( "a = %d , b = %d \n", a, b ) ;
+    printf( "\na = %d , b = %d \n", a, b ) ;
     printf( "a + b = %d\n", a + b ) ;
     printf( "a - b = %d\n", a - b) ;
     printf( "a * b = %d\n", a * b ) ;
