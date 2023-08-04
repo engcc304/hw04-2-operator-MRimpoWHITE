46bd5e66b04306fde6678bd09d21f7a69e28ce5c
MRimpoWHITE <phichitphu2015@gmail.com>
2023-08-05 01:42:14 +0700

    God nut

--- Homework/HW04/hw04-2-operator.cpp
+++ Homework/HW04/hw04-2-operator.cpp
@@ -3,10 +3,9 @@
 */
 #include <stdio.h>
 int main() {
-    int a = 0, b = 0 ,i ;
+    int a = 0, b = 0  ;
     printf( "Please enter value (a b) :" ) ;
     scanf( "%d %d", &a, &b ) ; //TODO: #11 Input variable 'a' and 'b'
-
     //TODO: #12 Complete operator, and display all operator output. (and fix all bugs.)
     printf( "\na = %d , b = %d \n", a, b ) ;
     printf( "a + b = %d\n", a + b ) ;
